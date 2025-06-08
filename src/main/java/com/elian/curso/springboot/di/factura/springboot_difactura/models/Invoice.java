cb4cd0c6c570a33afb71f7eb63045e8de9988ed4
elianguevara <elian.guevara689@gmail.com>
2025-06-08 19:22:04 -0300

    s

--- src/main/java/com/elian/curso/springboot/di/factura/springboot_difactura/models/Invoice.java
+++ src/main/java/com/elian/curso/springboot/di/factura/springboot_difactura/models/Invoice.java
@@ -7,9 +7,19 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import org.springframework.web.context.annotation.SessionScope;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 
 @Component
+@SessionScope
+@JsonIgnoreProperties({"targetSource","advisors", "targetClass", "proxyTargetClass", "staticPart", "this$0"})
 public class Invoice {
+
 
     @Autowired
     private Client client;
@@ -20,7 +30,20 @@ public class Invoice {
     @Qualifier("default")
     private List <Item> items;
 
-    
+    @PostConstruct
+    public void init(){
+        System.out.println("Creando el componente de la factura");
+        client.setName(client.getName().concat(" Pepe"));
+        description = description.concat(" del cliente").concat(client.getName().concat(" ").concat(client.getLastname()));
+        
+    }
+
+    @PreDestroy
+    public void destroy(){
+        System.out.println("Destruyendo el componente de la factura");
+        
+    }
+
     public Invoice() {
     }
     public Invoice(Client client, String description, List<Item> items) {
